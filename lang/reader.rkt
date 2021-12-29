1edd2ee26ec3683dcb1339d769c2cdab9bbda638
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2021-12-29 19:50:08 +0000

    spacecomma

--- lang/reader.rkt
+++ lang/reader.rkt
@@ -1,19 +1,19 @@
 #lang racket/base
 (require syntax/module-reader
-         (only-in "../readtable.rkt" make-csv-readtable))
+         (only-in "../readtable.rkt" make-spacecomma-readtable))
 
-(provide (rename-out [csv-read read]
-                     [csv-read-syntax read-syntax]
-                     [csv-get-info get-info]))
+(provide (rename-out [spacecomma-read read]
+                     [spacecomma-read-syntax read-syntax]
+                     [spacecomma-get-info get-info]))
 
 (define (wrap-reader p)
   (lambda args
-    (parameterize ([current-readtable (make-csv-readtable)])
+    (parameterize ([current-readtable (make-spacecomma-readtable)])
       (apply p args))))
 
-(define-values (csv-read csv-read-syntax csv-get-info)
+(define-values (spacecomma-read spacecomma-read-syntax spacecomma-get-info)
   (make-meta-reader
-   'csv  ;	self-sym
+   'spacecomma  ;	self-sym
    "language path" ; path-desc-str
    ; [	#:read-spec read-spec]	read-spec : (input-port? . -> . any/c) = (lambda (in) ....)
    lang-reader-module-paths ; module-path-parser : (any/c . -> . (or/c module-path? #f (vectorof module-path?)))
