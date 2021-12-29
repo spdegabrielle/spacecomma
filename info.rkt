792fc07f4d01993dd618606dbb4cdce41d5e0a1b
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2021-12-29 19:44:19 +0000

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,11 +1,11 @@
 #lang info
 
-(define collection "csv")
+(define collection "spacecomma")
 (define version "0.1")
 (define deps '(["base" #:version "8.3"]
                "scribble-lib"
                "syntax-color-lib"))
-(define pkg-desc "Implementation (no documentation) part of \"csv\"")
+(define pkg-desc "Implementation (no documentation) part of \"spacecomma\"")
 (define pkg-authors '(stephen))
 
 (define license
